f1f99ab691f63a60a08ba84d8eff64cf10851982
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-06-18 11:16:08 -0400

    add newReservation, Search, and NewTable routes

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,8 +2,11 @@ import React from "react";
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
+import NewReservation from "../components/reservations/NewReservation";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import Search from "../components/search/Search";
+import NewTable from "../components/tables/NewTable";
 
 /**
  * Defines all the routes for the application.
@@ -15,15 +18,34 @@ import { today } from "../utils/date-time";
 function Routes() {
   return (
     <Switch>
+      {/* DASHBOARD */}
       <Route exact={true} path="/">
-        <Redirect to={"/dashboard"} />
-      </Route>
-      <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
       </Route>
+
+      {/* RESERVATIONS */}
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations/new">
+        <NewReservation />
+      </Route>
+
+      {/* SEARCH */}
+      <Route exact={true} path="/search">
+        <Search />
+      </Route>
+
+      {/* TABLES */}
+
+      <Route exact={true} path="/tables/new">
+        <NewTable />
+      </Route>
+
+      {/* ERROR HANDLING */}
       <Route>
         <NotFound />
       </Route>
