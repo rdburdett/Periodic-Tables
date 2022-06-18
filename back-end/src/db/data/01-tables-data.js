72bcc7b21d6dfc71a54ba770ec394f212624e22f
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-06-18 11:13:49 -0400

    build seed data files

---
+++ back-end/src/db/data/01-tables-data.js
@@ -0,0 +1,23 @@
+
+module.exports = [
+  {
+    "table_name": "Bar #1",
+    "capacity": "1",
+    "status": "Available"
+  },
+  {
+    "table_name": "Bar #2",
+    "capacity": "1",
+    "status": "Available"
+  },
+  {
+    "table_name": "#1",
+    "capacity": "6",
+    "status": "Available"
+  },
+  {
+    "table_name": "#2",
+    "capacity": "6",
+    "status": "Available"
+  }
+]
