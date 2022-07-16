3bf385cec5a407f9dddf8549d1987078fdbfb8a2
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-07-15 21:18:14 -0400

    add listReservations

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
+import { listReservations, listTables } from "../utils/api";
+import useQuery from "../utils/useQuery";
+
 import ErrorAlert from "../layout/ErrorAlert";
 // import Card from "../components/reservations/Card.js";
 import Reservations from "../components/reservations/Reservations.js";
-
+import TableDetail from "../components/tables/TableDetail";
 
 /**
  * Defines the dashboard page.
@@ -13,18 +15,80 @@ import Reservations from "../components/reservations/Reservations.js";
  */
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
+  const [tables, setTables] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  const [tablesError, setTablesError] = useState(null);
 
-  useEffect(loadDashboard, [date]);
+  const query = useQuery();
+  const dateQuery = query.get("date");
 
-  function loadDashboard() {
-    const abortController = new AbortController();
-    setReservationsError(null);
-    listReservations({ date }, abortController.signal)
-      .then(setReservations)
-      .catch(setReservationsError);
-    return () => abortController.abort();
-  }
+  /////////
+
+  if (dateQuery) date = dateQuery;
+
+  // formats the date variable to be human readable
+  const dateObj = new Date(`${date} PDT`);
+  const dateString = dateObj.toDateString();
+
+  console.log(dateString)
+
+  /////////
+
+
+  // useEffect(() => {
+  //   loadDashboard()
+  // }, [date])
+  // useEffect(loadDashboard, [date]);
+
+  // function loadDashboard() {
+  //   const abortController = new AbortController();
+  //   setReservationsError(null);
+  //   listReservations({ date }, abortController.signal)
+  //     .then(setReservations)
+  //     .catch(setReservationsError);
+  //   listTables(abortController.signal)
+  //   .then(setTables)
+  //   .catch(setReservationsError);
+  //   return () => abortController.abort();
+  // }
+
+    ////////
+  
+    // Get request of reservations by date
+    useEffect(() => {
+      const abortController = new AbortController();
+  
+      async function loadReservations() {
+        setReservationsError(null);
+        try {
+          const data = await listReservations({ date }, abortController.signal);
+          setReservations(data);
+        } catch (error) {
+          setReservationsError(error);
+        }
+      }
+      loadReservations();
+      return () => abortController.abort();
+    }, [date]);
+
+    // loads all tables
+    useEffect(() => {
+      const abortController = new AbortController();
+  
+      async function loadTables() {
+        setReservationsError(null);
+        try {
+          const data = await listTables(abortController.signal);
+          setTables(data);
+        } catch (error) {
+          setTablesError(error);
+        }
+      }
+  
+      loadTables();
+  
+      return () => abortController.abort();
+    }, []);
 
   return (
     <main>
@@ -34,7 +98,14 @@ function Dashboard({ date }) {
       </div>
       <ErrorAlert error={reservationsError} />
       {/* {JSON.stringify(reservations)} */}
-      <Reservations reservations={reservations}/>
+      <Reservations reservations={reservations} />
+      <div className="mb-3 mx-3"> 
+          <div className="headingBar my-3 p-2">
+              <h2>Tables</h2>
+          </div>
+            <ErrorAlert error={tablesError} />
+            <TableDetail tables={tables} />
+        </div>
     </main>
   );
 }
