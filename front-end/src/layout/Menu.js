571c300141f90aabda7061fe799a5be59a7bc912
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-07-21 00:46:04 -0400

    navbar improvements

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -12,7 +12,7 @@ function Menu() {
     <nav className="navbar navbar-dark align-items-start p-0 my-3">
       <div className="container-fluid d-flex flex-column p-0">
         <Link
-          className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
+          className="navbar-brand justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
           <div className="sidebar-brand-text mx-3">
@@ -20,26 +20,26 @@ function Menu() {
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
+        <ul className="nav navbar-nav text-light d-flex flex-row" id="accordionSidebar">
+          <li className="nav-item p-1">
             <Link className="nav-link" to="/dashboard">
               <span className="oi oi-dashboard" />
               &nbsp;Dashboard
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item p-1">
             <Link className="nav-link" to="/search">
               <span className="oi oi-magnifying-glass" />
               &nbsp;Search
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item p-1">
             <Link className="nav-link" to="/reservations/new">
               <span className="oi oi-plus" />
               &nbsp;New Reservation
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item p-1">
             <Link className="nav-link" to="/tables/new">
               <span className="oi oi-layers" />
               &nbsp;New Table
