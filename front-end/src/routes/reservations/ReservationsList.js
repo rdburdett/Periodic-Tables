d544bac0c57161f63408b92c216be10134d1d586
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-07-25 20:45:44 -0400

    add div

--- front-end/src/routes/reservations/ReservationsList.js
+++ front-end/src/routes/reservations/ReservationsList.js
@@ -3,28 +3,32 @@ import ReservationCard from "./ReservationCard";
 import NewCard from "./NewCard";
 
 function ReservationsList({ reservations = [] }) {
-    
     const reservationList = (
         // Main list column
-        <div id="reservation-mapped-list" className="card-columns">
-            {/* Map reservation cards, filtering out 'cancelled' reservations */}
-            {reservations.map(
-                (reservation, index) =>
-                reservation.status !== "cancelled" && (
-                    <ReservationCard
-                    key={reservation.reservation_id}
-                    reservation={reservation}
-                    index={index}
-                    />
-                    )
-            )}
+        <div>
+            <div id="reservation-mapped-list" className="card-columns">
+                {/* Map reservation cards, filtering out 'cancelled' reservations */}
+                {reservations.map(
+                    (reservation, index) =>
+                        reservation.status !== "cancelled" && (
+                            <ReservationCard
+                                key={reservation.reservation_id}
+                                reservation={reservation}
+                                index={index}
+                            />
+                        )
+                )}
             <NewCard />
+            </div>
+
         </div>
     );
 
     const addNewCard = (
-        <div id="add-card">
-            <p className="text-secondary">No reservations found for this date.</p>
+        <div id="add-card" className="card-columns">
+            <p className="text-secondary">
+                No reservations found for this date.
+            </p>
             <NewCard />
         </div>
     );
