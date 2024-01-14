1cde770e7ee0cf29a531b77a8409053fd7237032
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2024-01-14 12:14:06 -0600

    improve "new reservation"

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -11,6 +11,7 @@ function Menu() {
     return (
         <nav className="navbar navbar-light align-items-start mt-3 w-100">
             <div className="container-fluid d-flex flex-column p-1">
+                {/* PERIODIC TABLES HEADER */}
                 <Link
                     className="navbar-brand justify-content-center align-items-center sidebar-brand m-0"
                     to="/"
@@ -21,10 +22,13 @@ function Menu() {
                     </div>
                 </Link>
                 <hr className="sidebar-divider my-0" />
+                
+                {/* MAIN MENU */}
                 <ul
                     className="nav navbar-nav text-light d-flex flex-row flex-lg-column flex-xl-column justify-content-center"
                     id="accordionSidebar"
                 >
+                    {/* Dashboard */}
                     <div className="row mx-1">
                         <li className="nav-item mr-3">
                             <Link
@@ -36,21 +40,22 @@ function Menu() {
                             </Link>
                         </li>
                     </div>
+
+                    {/* Search */}
                     <div className="row mx-1">
                         <li className="nav-item mr-3">
                             <Link className="nav-link text-light" to="/search">
-                                <span className="mr-2 oi oi-magnifying-glass" />
-                                Search
+                                <span className="mr-2 oi oi-magnifying-glass" />Search
                             </Link>
                         </li>
                     </div>
 
+                    {/* New Reservation */}
                     <div className="row mx-1">
                         <li className="nav-item mr-3">
                             <Link
                                 className="nav-link text-light"
                                 to="/reservations/new"
-                                style={{width: 140}}
                             >
                                 <span className="mr-2 oi oi-plus" />
                                 New Reservation
@@ -58,6 +63,7 @@ function Menu() {
                         </li>
                     </div>
 
+                    {/* New Table */}
                     <div className="row mx-1">
                         <li className="nav-item">
                             <Link
